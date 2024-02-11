9ebb2eb6191aa6f4df46dcdad4817250d14b3697
jiehuan233 <jiehuan233@outlook.com>
2024-02-11 20:44:07 +0800

    添加项目文件。

---
+++ HelloWorld/MainPage.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.WebUI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Maps;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.Web.Http;
+using HtmlAgilityPack;
+using System.Diagnostics;
+using Windows.UI.Xaml.Media.Imaging;
+
+// https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
+
+namespace HelloWorld
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private async void FetchAndDisplayImage()
+        {
+            // 使用 HttpClient 获取 HTML 内容
+            using (HttpClient client = new HttpClient())
+            {
+                string htmlContent = await client.GetStringAsync(new Uri("https://tieba.baidu.com/mo/q---F01A6832500E30E2BE965B337275AA09%3AFG%3D1--1-3-0--2--wapp_1608480082014_787/m?tn=bdIndex&lp=5014")); // 替换为你要抓取的网页 URL
+
+                // 使用 HtmlAgilityPack 解析 HTML
+                HtmlDocument htmlDoc = new HtmlDocument();
+                htmlDoc.LoadHtml(htmlContent);
+
+                // 使用 XPath 选择图片节点
+                HtmlNode imgNode = htmlDoc.DocumentNode.SelectSingleNode("/html/body/div[1]/img");
+
+                // 提取图片 URL
+                string imageUrl = "https:"+imgNode?.GetAttributeValue("src", "");
+                Debug.WriteLine("imageUrl");
+
+                // 显示图片
+                if (!string.IsNullOrEmpty(imageUrl))
+                {
+                    
+                    ImageView.Source = new Windows.UI.Xaml.Media.Imaging.BitmapImage(new Uri(imageUrl));//imageUrl
+                }
+            }
+        }
+        public MainPage()
+        {
+            this.InitializeComponent();
+            FetchAndDisplayImage();
+            text.Visibility = Visibility.Collapsed;
+            
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            text.Visibility = Visibility.Visible;
+            //LoadWebPage();
+            //button.Visibility = Visibility.Collapsed;
+        }
+
+        private void tieba_LoadCompleted(object sender, NavigationEventArgs e)
+        {
+
+        }
+    }
+}
